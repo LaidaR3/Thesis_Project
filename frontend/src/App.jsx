eb1004fbf125958d87ce2c36f458851832436cf8
Laida Rusinovci <149337343+LaidaR3@users.noreply.github.com>
2026-01-01 22:18:32 +0100

    Add protected route infrastructure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import RequireAuth from "./auth/RequireAuth";
+import Public from "./pages/Public";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
-  return <h1>Zero Trust Microservices Frontend</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Public />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
